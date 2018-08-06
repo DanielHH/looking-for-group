3107ff3281d3a6e42682436a43040c17d66b906f
Eric Nylander <eric.nylander@live.com>
2018-08-06 12:52:21 +0200

    initial changes to unittests and server setup. Added pycache to .gitignore

--- backend/setup.py
+++ backend/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='LabApp',
+setup(name='looking-for-group',
       version='1.0',
       description='OpenShift App',
       author='Eric Nylander',
       author_email='eriny656@student.liu.se',
       url='http://labapp-eriny656.openshift.ida.liu.se/',
-      install_requires=['Flask', 'Flask-SQLAlchemy>=2.1', 'sqlalchemy>=1.0.11'],
+      install_requires=['Flask', 'Flask-SQLAlchemy>=2.1', 'sqlalchemy>=1.1.4'],
      )
--- backend/test.py
+++ backend/test.py
@@ -12,11 +12,11 @@ class DataTest(unittest.TestCase):
         server.db.create_all()
 
     def post_messages(self, token):
-        self.server.post('/messages', headers={'Content-Type':'application/json', "Authorization":token},
+        self.server.post('/messages', headers={'Content-Type': 'application/json', "Authorization": token},
                          data=json.dumps('A message'))
-        self.server.post('/messages', headers={'Content-Type':'application/json', "Authorization":token},
+        self.server.post('/messages', headers={'Content-Type': 'application/json', "Authorization": token},
                          data=json.dumps('A second message'))
-        self.server.post('/messages', headers={'Content-Type':'application/json', "Authorization":token},
+        self.server.post('/messages', headers={'Content-Type': 'application/json', "Authorization": token},
                          data=json.dumps('Another message'))
 
     def convert_to_literal(self, rv):
@@ -25,17 +25,17 @@ class DataTest(unittest.TestCase):
         return rv
 
     def create_users(self):
-        self.server.post('/user', headers={'Content-Type':'application/json'},
-                         data=json.dumps({'username':'user','password':'password'}))
-        self.server.post('/user', headers={'Content-Type':'application/json'},
-                         data=json.dumps({'username':'eriny656','password':'password'}))
-        self.server.post('/user', headers={'Content-Type':'application/json'},
-                         data=json.dumps({'username':'somon123','password':'password'}))
+        self.server.post('/user', headers={'Content-Type': 'application/json'},
+                         data=json.dumps({'email': 'user@email.com', 'name': 'user', 'password': 'password'}))
+        self.server.post('/user', headers={'Content-Type': 'application/json'},
+                         data=json.dumps({'email': 'eriny656@student.liu.se', 'name': 'eriny656', 'password': 'password'}))
+        self.server.post('/user', headers={'Content-Type': 'application/json'},
+                         data=json.dumps({'email': 'somon123@student.liu.se', 'name': 'somon123', 'password': 'password'}))
 
-    def login_user(self, username):
-        self.server.post('/user/login', headers={'Content-Type':'application/json'},
-                         data=json.dumps({'username':'{0}'.format(username),'password':'password'}))
-        user = server.User.query.filter_by(username='{0}'.format(username)).first()
+    def login_user(self, email):
+        self.server.post('/user/login', headers={'Content-Type': 'application/json'},
+                         data=json.dumps({'email': '{0}'.format(email), 'password': 'password'}))
+        user = server.User.query.filter_by(email='{0}'.format(email)).first()
         token = server.Token.query.filter_by(user_id=user.id).first()
         return token.token
 
@@ -44,26 +44,26 @@ class DataTest(unittest.TestCase):
         users = server.User.query.all()
         user_list = []
         for user in users:
-            user_list.append(user.username)
+            user_list.append(user.name)
         self.assertEqual(user_list, ['user', 'eriny656', 'somon123'], "Failed in create_users")
 
     def test_login_user(self):
         self.create_users()
-        token = self.login_user('user')
-        user = server.User.query.filter_by(username='user').first()
+        token = self.login_user('user@email.com')
+        user = server.User.query.filter_by(name='user').first()
         test_token = server.Token.query.filter_by(user_id=user.id).first()
         self.assertEqual(token, str(test_token), "Failed in login_user")
 
     def test_logout_user(self):
         self.create_users()
-        token = self.login_user('user')
-        self.server.post('/user/logout', headers={"Authorization":token})
+        token = self.login_user('user@email.com')
+        self.server.post('/user/logout', headers={"Authorization": token})
         tokens = server.Token.query.all()
         self.assertEqual(tokens, [])
 
     def test_post_messages(self):
         self.create_users()
-        token = self.login_user('user')
+        token = self.login_user('user@email.com')
         self.post_messages(token)
         messages = server.Message.query.all()
         message_list = []
@@ -73,14 +73,14 @@ class DataTest(unittest.TestCase):
 
     def test_get_messages(self):
         self.create_users()
-        token = self.login_user('user')
+        token = self.login_user('user@email.com')
         self.post_messages(token)
         rv = self.convert_to_literal(self.server.get('/messages'))
         self.assertEqual('A second message', rv[1]["message"], "failed in get_messages")
 
     def test_get_message_id(self):
         self.create_users()
-        token = self.login_user('user')
+        token = self.login_user('user@email.com')
         self.post_messages(token)
         rv = self.convert_to_literal(self.server.get('/messages'))
         message_two_id = rv[1]["id"]
@@ -89,20 +89,20 @@ class DataTest(unittest.TestCase):
 
     def test_flag_as_read(self):
         self.create_users()
-        user_token = self.login_user('user')
-        somon123_token = self.login_user('somon123')
+        user_token = self.login_user('user@email.com')
+        somon123_token = self.login_user('somon123@student.liu.se')
         self.post_messages(user_token)
         rv = self.convert_to_literal(self.server.get('/messages'))
         message_one_id = rv[0]["id"]
         message_two_id = rv[1]["id"]
-        self.server.post('/user/logout', headers={"Authorization":user_token})
-        eriny656_token = self.login_user('eriny656')
+        self.server.post('/user/logout', headers={"Authorization": user_token})
+        eriny656_token = self.login_user('eriny656@student.liu.se')
         self.server.post('/messages/{0}/flag/eriny656'.format(message_one_id),
-                         headers={'Authorization':eriny656_token})
-        self.server.post('/user/logout', headers={"Authorization":eriny656_token})
+                         headers={'Authorization': eriny656_token})
+        self.server.post('/user/logout', headers={"Authorization": eriny656_token})
         self.server.post('/messages/{0}/flag/somon123'.format(message_two_id),
-                         headers={'Authorization':somon123_token})
-        self.server.post('/user/logout', headers={"Authorization":somon123_token})
+                         headers={'Authorization': somon123_token})
+        self.server.post('/user/logout', headers={"Authorization": somon123_token})
         rv = self.convert_to_literal(self.server.get('/messages'))
         self.assertEqual(['eriny656'], rv[0]['read_by'], "failed in flag_as_read")
         self.assertEqual(['somon123'], rv[1]['read_by'], "failed in flag_as_read")
@@ -115,9 +115,9 @@ class DataTest(unittest.TestCase):
         rv = self.convert_to_literal(self.server.get('/messages'))
         message_one_id = rv[0]["id"]
         message_two_id = rv[1]["id"]
-        self.server.post('/messages/{0}/flag/user'.format(message_one_id), headers={"Authorization":token})
-        self.server.post('/messages/{0}/flag/user'.format(message_two_id), headers={"Authorization":token})
-        rv = self.convert_to_literal(self.server.get('/messages/unread/user', headers={"Authorization":token}))
+        self.server.post('/messages/{0}/flag/user'.format(message_one_id), headers={"Authorization": token})
+        self.server.post('/messages/{0}/flag/user'.format(message_two_id), headers={"Authorization": token})
+        rv = self.convert_to_literal(self.server.get('/messages/unread/user', headers={"Authorization": token}))
         self.assertEqual('Another message', rv[0]['message'], "failed in get_unread")
 
     def test_delete_message(self):
@@ -128,8 +128,10 @@ class DataTest(unittest.TestCase):
         message_one_id = rv[0]["id"]
         message_two_id = rv[1]["id"]
         message_three_id = rv[2]["id"]
-        self.server.delete('http://127.0.0.1:5000/messages/{0}'.format(message_one_id), headers={"Authorization":token})
-        self.server.delete('http://127.0.0.1:5000/messages/{0}'.format(message_two_id), headers={"Authorization":token})
+        self.server.delete('http://127.0.0.1:5000/messages/{0}'.format(message_one_id),
+                           headers={"Authorization": token})
+        self.server.delete('http://127.0.0.1:5000/messages/{0}'.format(message_two_id),
+                           headers={"Authorization": token})
         rv = self.convert_to_literal(self.server.get('/messages'))
         message_three = self.convert_to_literal(self.server.get('/messages/{0}'.format(message_three_id)))
         self.assertEqual(rv[0], message_three, "Failed in delete_message")
@@ -137,20 +139,20 @@ class DataTest(unittest.TestCase):
     def test_long_message_post(self):
         self.create_users()
         token = self.login_user('user')
-        rv = self.server.post('/messages', headers={'Content-Type':'application/json',
-                                                    'Authorization':token}, data=json.dumps('A message longer '+
-                'than 140 characters..............................................................................'+
+        rv = self.server.post('/messages', headers={'Content-Type': 'application/json',
+                                                    'Authorization': token}, data=json.dumps('A message longer ' +
+                'than 140 characters..............................................................................' +
                 '................................'))
         self.assertEqual(rv.status_code, 400, 'Failed in message > 140 characters')
 
     def test_messages_delete(self):
         self.create_users()
         token = self.login_user('user')
-        resp = self.server.delete('/messages', headers={'Authorization':token})
+        resp = self.server.delete('/messages', headers={'Authorization': token})
         self.assertEqual(resp.status_code, 405, 'Failed in improper method to /messages')
 
     def test_bad_signature(self):
         self.create_users()
-        rv = self.server.post("/messages", headers={'Content-Type':'application/json', 'Authorization':'incorrect'},
+        rv = self.server.post("/messages", headers={'Content-Type': 'application/json', 'Authorization': 'incorrect'},
                               data=json.dumps('invisible message'))
         self.assertEqual(rv.status_code, 401, 'Failed in bad_signature')
